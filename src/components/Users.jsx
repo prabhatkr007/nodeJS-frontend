4206f7ee2a796a280e3fdbac3d0303eda1149011
prabhatkr007 <pkr.876550@gmail.com>
2023-04-23 17:56:52 +0530

    connected frontend to server

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,21 +1,19 @@
-
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-
-function App() {
-  
+function UserTable() {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    fetch('http://127.0.0.1:5000/users/income-lower-than-5-with-bmw-mercedes')
-      .then((response) => response.json())
-      .then((data) => setUsers(data));
+    axios.get('http://127.0.0.1:5000/users/income-lower-than-5-with-bmw-mercedes').then((response) => {
+      setUsers(response.data);
+    });
   }, []);
 
-  return(
-  <table>
-  <thead>
-    <tr>
+  return (
+    <table>
+      <thead>
+        <tr>
           <th>First Name</th>
           <th>Last Name</th>
           <th>Email</th>
@@ -25,26 +23,25 @@ function App() {
           <th>Car Model</th>
           <th>Quote</th>
           <th>Phone Price</th>
-    </tr>
-  </thead>
-  <tbody>
-    {users.map((user) => (
-      <tr key={user.id}>
-        <td>{user.first_name}</td>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((user) => (
+          <tr key={user.id}>
+            <td>{user.first_name}</td>
             <td>{user.last_name}</td>
             <td>{user.email}</td>
             <td>{user.gender}</td>
             <td>{user.income}</td>
             <td>{user.city}</td>
-            <td>{user.car}</td>
+            <td>{user.car_model}</td>
             <td>{user.quote}</td>
             <td>{user.phone_price}</td>
-      </tr>
-    ))}
-  </tbody>
-</table>
-);
-
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
 
-export default App;
+export default UserTable;
